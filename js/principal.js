2fc79021c93d5da6e658a38295a4ab60b5b091e7
Leonardo Queiroz Pinheiro - SUPCD/CDCEL/CDCEB <leonardo.q.pinheiro@gmail.com>
2017-05-19 14:15:14 -0300

    Alteração do JS

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,7 @@
 //console.log("carregando do arquivo");
-
 var titulo=document.querySelector('#titulo');
 
 //console.log(titulo.textContent);
-
 titulo.textContent="Aparecida Nutricionista";
+
+//Novas alterações para a branch javascript-v0.1
