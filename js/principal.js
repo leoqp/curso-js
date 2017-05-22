3c13fcc4827eef0a2c979003cace6529216b12e5
Leonardo Queiroz Pinheiro - SUPCD/CDCEL/CDCEB <leonardo.q.pinheiro@gmail.com>
2017-05-22 12:42:06 -0300

    Inclusão de validação de peso e altura

--- js/principal.js
+++ js/principal.js
@@ -4,12 +4,29 @@ var titulo=document.querySelector('#titulo');
 var trPaciente=document.querySelector('#primeiro-paciente');
 var pesoPaciente = trPaciente.querySelector('.info-peso').textContent;
 var alturaPaciente = trPaciente.querySelector('.info-altura').textContent;
-
-var calculoImc= pesoPaciente/(alturaPaciente*alturaPaciente);
-console.log(calculoImc);
-
 var imcPaciente = trPaciente.querySelector('.info-imc');
-imcPaciente.textContent=calculoImc;
+
+var pesoValidador=true;
+var alturaValidador=true;
+
+if((pesoPaciente<=0) || (pesoPaciente>=300))
+{
+  pesoValidador=false;
+}
+
+if((alturaPaciente <= 0) || (alturaPaciente > 3.00))
+{
+  alturaValidador=false;
+}
+
+if(alturaValidador && pesoValidador)
+{
+  var calculoImc= pesoPaciente/(alturaPaciente*alturaPaciente);
+  imcPaciente.textContent=calculoImc;
+}
+else {
+  imcPaciente.textContent="Dados inválidos!";
+}
 //console.log(tituloPacientes.textContent);
 
 //console.log(titulo.textContent);
