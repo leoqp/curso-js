1418601df6dc61b1be6dc01801d0a1f23af77326
Leonardo Queiroz Pinheiro - SUPCD/CDCEL/CDCEB <leonardo.q.pinheiro@gmail.com>
2017-05-22 13:56:12 -0300

    Alterações para calculo de todos os pacientes

--- js/principal.js
+++ js/principal.js
@@ -1,33 +1,42 @@
-//console.log("carregando do arquivo");
 var titulo=document.querySelector('#titulo');
 
-var trPaciente=document.querySelector('#primeiro-paciente');
-var pesoPaciente = trPaciente.querySelector('.info-peso').textContent;
-var alturaPaciente = trPaciente.querySelector('.info-altura').textContent;
-var imcPaciente = trPaciente.querySelector('.info-imc');
+//Busca todos os pacientes com a classe paciente
+var pacientes = document.querySelectorAll('.paciente');
 
-var pesoValidador=true;
-var alturaValidador=true;
-
-if((pesoPaciente<=0) || (pesoPaciente>=300))
+//For de 0 até o indice do último paciente
+for (var i=0;i < pacientes.length;i++)
 {
-  pesoValidador=false;
+  var trPaciente = pacientes[i];
+  var pesoPaciente = trPaciente.querySelector('.info-peso').textContent;
+  var alturaPaciente = trPaciente.querySelector('.info-altura').textContent;
+  var imcPaciente = trPaciente.querySelector('.info-imc');
+
+  var pesoValidador=true;
+  var alturaValidador=true;
+
+  if((pesoPaciente<=0) || (pesoPaciente>=300))
+  {
+    pesoValidador=false;
+  }
+
+  if((alturaPaciente <= 0) || (alturaPaciente > 3.00))
+  {
+    alturaValidador=false;
+  }
+
+  if(alturaValidador && pesoValidador)
+  {
+    var calculoImc= pesoPaciente/(alturaPaciente*alturaPaciente);
+    //Traz apenas 2 casas decimais do numéro calculado
+    imcPaciente.textContent=calculoImc.toFixed(2);
+  }
+  else {
+    imcPaciente.textContent="Dados inválidos!";
+  }
+  //console.log(tituloPacientes.textContent);
 }
 
-if((alturaPaciente <= 0) || (alturaPaciente > 3.00))
-{
-  alturaValidador=false;
-}
 
-if(alturaValidador && pesoValidador)
-{
-  var calculoImc= pesoPaciente/(alturaPaciente*alturaPaciente);
-  imcPaciente.textContent=calculoImc;
-}
-else {
-  imcPaciente.textContent="Dados inválidos!";
-}
-//console.log(tituloPacientes.textContent);
 
 //console.log(titulo.textContent);
 titulo.textContent="Aparecida Nutricionista";
